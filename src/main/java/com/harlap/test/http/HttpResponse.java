e10748cc79bf9dca6991ec79865a06f94b5e4b35
Kristof A <kr_adr@yahoo.co.uk>
2012-10-21 15:43:28 +0200

    Oops, forgot to check-in HttpResponse. Extracted and adapted from MockHttpServer.

---
+++ src/main/java/com/harlap/test/http/HttpResponse.java
@@ -0,0 +1,31 @@
+package com.harlap.test.http;
+
+/**
+ * HTTP response.
+ * 
+ * @author kristof
+ */
+public interface HttpResponse {
+
+    /**
+     * Gets a HTTP response code.
+     * 
+     * @return Gets a HTTP response code.
+     */
+    int getHttpCode();
+
+    /**
+     * Get content type.
+     * 
+     * @return Get content type.
+     */
+    String getContentType();
+
+    /**
+     * Get response content.
+     * 
+     * @return Response content.
+     */
+    byte[] getContent();
+
+}
---
+++ src/main/java/com/harlap/test/http/HttpResponseImpl.java
@@ -0,0 +1,57 @@
+package com.harlap.test.http;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class HttpResponseImpl implements HttpResponse {
+
+    private final int httpCode;
+    private final String contentType;
+    private final byte[] content;
+
+    public HttpResponseImpl(final int httpCode, final String contentType, final byte[] content) {
+        this.httpCode = httpCode;
+        this.contentType = contentType;
+        this.content = content;
+    }
+
+    @Override
+    public int getHttpCode() {
+        return httpCode;
+    }
+
+    @Override
+    public String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public byte[] getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return "Http code: " + getHttpCode() + ", Content Type: " + (getContentType() == null ? "null" : getContentType())
+            + ", Content: " + (getContent() == null ? "null" : new String(getContent()));
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this, false);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj, false);
+    }
+
+}
