f7d347eb1dea2f029de70851b25c7227e2446028
Kristof A <kr_adr@yahoo.co.uk>
2012-12-09 16:37:44 +0100

    Add javadocs for HttpRequestException.

--- src/main/java/com/github/kristofa/test/http/client/HttpRequestException.java
+++ src/main/java/com/github/kristofa/test/http/client/HttpRequestException.java
@@ -1,13 +1,28 @@
 package com.github.kristofa.test.http.client;
 
+/**
+ * Exception that indicates that HttpRequest failed.
+ * 
+ * @author kristof
+ */
 public class HttpRequestException extends Exception {
 
     private static final long serialVersionUID = -3214795946755533575L;
 
+    /**
+     * Creates a new instance.
+     * 
+     * @param throwable Causing exception.
+     */
     public HttpRequestException(final Throwable throwable) {
         super(throwable);
     }
 
+    /**
+     * Creates a new instance.
+     * 
+     * @param message Exception message.
+     */
     public HttpRequestException(final String message) {
         super(message);
     }
