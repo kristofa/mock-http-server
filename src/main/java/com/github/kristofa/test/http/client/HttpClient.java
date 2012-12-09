4158f324f96a5be8483c823e4e44c1cdb139ed15
Kristof A <kr_adr@yahoo.co.uk>
2012-12-09 16:37:07 +0100

    Add javadocs for HttpClient.

--- src/main/java/com/github/kristofa/test/http/client/HttpClient.java
+++ src/main/java/com/github/kristofa/test/http/client/HttpClient.java
@@ -11,6 +11,13 @@ import com.github.kristofa.test.http.FullHttpRequest;
  */
 public interface HttpClient {
 
+    /**
+     * Executes a HTTP request and returns response.
+     * 
+     * @param request HTTP request.
+     * @return Response.
+     * @throws HttpRequestException In case request failed.
+     */
     HttpClientResponse<InputStream> execute(final FullHttpRequest request) throws HttpRequestException;
 
 }
