ca7a8b5e17062c1b3db612f0eb2978ac6f4375d6
Kristof A <kr_adr@yahoo.co.uk>
2012-12-16 18:03:01 +0100

    Add HttpRequestFileReaderImpl and tests.

---
+++ src/main/java/com/github/kristofa/test/http/HttpRequestFileReaderImpl.java
@@ -0,0 +1,111 @@
+package com.github.kristofa.test.http;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Builds a HttpRequest for which the content is stored on disk. It can reconstruct a HttpRequest which was previously stored
+ * with {@link HttpRequestFileWriterImpl}.
+ * 
+ * @see HttpRequestFileWriterImpl
+ * @author kristof
+ */
+class HttpRequestFileReaderImpl implements HttpRequestFileReader {
+
+    private static final String QUERY_PARAMETERS = "[QueryParameters]";
+    private static final String PATH = "[Path]";
+    private static final String HTTP_MESSAGE_HEADER = "[HttpMessageHeader]";
+    private static final String METHOD = "[Method]";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public HttpRequest read(final File httpRequestFile, final File httpRequestEntityFile) {
+        final HttpRequestImpl httpRequestImpl = new HttpRequestImpl();
+        try {
+            readRequest(httpRequestImpl, httpRequestFile, httpRequestEntityFile);
+        } catch (final IOException e) {
+            throw new IllegalStateException(e);
+        }
+        return httpRequestImpl;
+    }
+
+    private void readRequest(final HttpRequestImpl request, final File requestFile, final File requestEntityFile)
+        throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(requestFile), "UTF-8"));
+        try {
+            readNextLine(reader, METHOD);
+            final String method = reader.readLine();
+            request.method(Method.valueOf(method));
+            readNextLine(reader, HTTP_MESSAGE_HEADER);
+            final List<KeyValuePair> httpMessageHeaderValues = new ArrayList<KeyValuePair>();
+            final String nextSection = readKeyValuePairs(reader, httpMessageHeaderValues);
+            if (!PATH.equals(nextSection)) {
+                throw new IllegalStateException("Expected " + PATH + " after " + HTTP_MESSAGE_HEADER + " but got "
+                    + nextSection);
+            }
+            for (final KeyValuePair keyValuePair : httpMessageHeaderValues) {
+                request.httpMessageHeader(keyValuePair.key, keyValuePair.value);
+            }
+            final String path = reader.readLine();
+            request.path(path);
+            readNextLine(reader, QUERY_PARAMETERS);
+            final List<KeyValuePair> queryParams = new ArrayList<KeyValuePair>();
+            final String nextSection2 = readKeyValuePairs(reader, queryParams);
+            if (nextSection2 != null) {
+                throw new IllegalStateException("Expected nothing after " + QUERY_PARAMETERS + " but got " + nextSection2);
+            }
+            for (final KeyValuePair keyValuePair : queryParams) {
+                request.queryParameter(keyValuePair.key, keyValuePair.value);
+            }
+
+            if (requestEntityFile.exists()) {
+                final byte[] entity = FileUtils.readFileToByteArray(requestEntityFile);
+                request.content(entity);
+            }
+
+        } finally {
+            reader.close();
+        }
+    }
+
+    private String readNextLine(final BufferedReader reader, final String expectedValue) throws IOException {
+        final String value = reader.readLine();
+        if (!expectedValue.equals(value)) {
+            throw new IllegalStateException("Unexpected value. Expected " + expectedValue + " but was " + value);
+        }
+        return value;
+    }
+
+    private String readKeyValuePairs(final BufferedReader reader, final List<KeyValuePair> properties) throws IOException {
+        String newLine = null;
+        while ((newLine = reader.readLine()) != null) {
+            if (newLine.indexOf("=") != -1) {
+                final String[] split = newLine.split("=");
+                final KeyValuePair pair = new KeyValuePair();
+                pair.key = split[0];
+                pair.value = split[1];
+                properties.add(pair);
+            } else {
+                return newLine;
+            }
+        }
+        return null;
+    }
+
+    private class KeyValuePair {
+
+        public String key;
+        public String value;
+
+    }
+
+}
---
+++ src/test/java/com/github/kristofa/test/http/HttpRequestFileReaderTest.java
@@ -0,0 +1,98 @@
+package com.github.kristofa.test.http;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HttpRequestFileReaderTest {
+
+    private final static String TEST_FILE_DIRECTORY = "target/test-classes/";
+
+    private HttpRequestFileReaderImpl reader;
+
+    @Before
+    public void setup() {
+        reader = new HttpRequestFileReaderImpl();
+    }
+
+    @Test
+    public void testReadValidRequestNoEntity() {
+
+        final HttpRequest request =
+            reader.read(new File(TEST_FILE_DIRECTORY, "HttpRequestFileReaderTest_valid_file.txt"), new File(
+                "unexisting_file.txt"));
+        assertEquals(Method.POST, request.getMethod());
+        assertEquals("/a/b", request.getPath());
+        final Set<HttpMessageHeader> httpMessageHeaders = request.getHttpMessageHeaders();
+        assertEquals(2, httpMessageHeaders.size());
+        assertTrue(httpMessageHeaders.contains(new HttpMessageHeader("Content-Type", "application/json")));
+        assertTrue(httpMessageHeaders.contains(new HttpMessageHeader("Agent", "Eclipse")));
+        final Set<QueryParameter> queryParameters = request.getQueryParameters();
+        assertEquals(3, queryParameters.size());
+        assertTrue(queryParameters.contains(new QueryParameter("a", "b")));
+        assertTrue(queryParameters.contains(new QueryParameter("a", "c")));
+        assertTrue(queryParameters.contains(new QueryParameter("c", "d")));
+        assertNull(request.getContent());
+    }
+
+    @Test
+    public void testReadValidRequestWithEntity() {
+
+        final HttpRequest request =
+            reader.read(new File(TEST_FILE_DIRECTORY, "HttpRequestFileReaderTest_valid_file.txt"), new File(
+                TEST_FILE_DIRECTORY, "HttpRequestFileReaderTest_entity_valid_file.txt"));
+        assertEquals(Method.POST, request.getMethod());
+        assertEquals("/a/b", request.getPath());
+        final Set<HttpMessageHeader> httpMessageHeaders = request.getHttpMessageHeaders();
+        assertEquals(2, httpMessageHeaders.size());
+        assertTrue(httpMessageHeaders.contains(new HttpMessageHeader("Content-Type", "application/json")));
+        assertTrue(httpMessageHeaders.contains(new HttpMessageHeader("Agent", "Eclipse")));
+        final Set<QueryParameter> queryParameters = request.getQueryParameters();
+        assertEquals(3, queryParameters.size());
+        assertTrue(queryParameters.contains(new QueryParameter("a", "b")));
+        assertTrue(queryParameters.contains(new QueryParameter("a", "c")));
+        assertTrue(queryParameters.contains(new QueryParameter("c", "d")));
+        assertNotNull(request.getContent());
+    }
+
+    @Test
+    public void testReadInvalidFile_NotStartWithMethod() {
+        try {
+            reader.read(new File(TEST_FILE_DIRECTORY, "HttpRequestFileReaderTest_invalidFile1.txt"), new File(
+                TEST_FILE_DIRECTORY, "HttpRequestFileReaderTest_entity_valid_file.txt"));
+            fail("Expected exception.");
+        } catch (final IllegalStateException e) {
+            assertEquals("Unexpected value. Expected [Method] but was [HttpMessageHeader]", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testReadInvalidFile_EndOfFileAfterHttpMessageHeader() {
+        try {
+            reader.read(new File(TEST_FILE_DIRECTORY, "HttpRequestFileReaderTest_invalidFile2.txt"), new File(
+                TEST_FILE_DIRECTORY, "HttpRequestFileReaderTest_entity_valid_file.txt"));
+            fail("Expected exception.");
+        } catch (final IllegalStateException e) {
+            assertEquals("Expected [Path] after [HttpMessageHeader] but got null", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testReadInvalidFile_UnexpectedContentAfterQueryParams() {
+        try {
+            reader.read(new File(TEST_FILE_DIRECTORY, "HttpRequestFileReaderTest_invalidFile3.txt"), new File(
+                TEST_FILE_DIRECTORY, "HttpRequestFileReaderTest_entity_valid_file.txt"));
+            fail("Expected exception.");
+        } catch (final IllegalStateException e) {
+            assertEquals("Expected nothing after [QueryParameters] but got [UnexpectedSection]", e.getMessage());
+        }
+    }
+}
