40a76fcdc2cb8a2e37871eff5bc7288a5e1367d7
Kristof A <kr_adr@yahoo.co.uk>
2012-12-17 19:01:44 +0100

    Add HttpResponseFileWriterImpl

---
+++ src/main/java/com/github/kristofa/test/http/HttpResponseFileWriterImpl.java
@@ -0,0 +1,50 @@
+package com.github.kristofa.test.http;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import org.apache.commons.io.FileUtils;
+
+class HttpResponseFileWriterImpl implements HttpResponseFileWriter {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void write(final HttpResponse response, final File httpResponseFile, final File httpResponseEntityFile) {
+        try {
+            writeResponse(response, httpResponseFile);
+            writeResponseEntity(response, httpResponseEntityFile);
+        } catch (final IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void writeResponse(final HttpResponse httpResponse, final File httpResponseFile) throws IOException {
+        final BufferedWriter writer =
+            new BufferedWriter(new OutputStreamWriter(new FileOutputStream(httpResponseFile), "UTF-8"));
+        try {
+            writer.write("[HttpCode]");
+            writer.newLine();
+            writer.write(String.valueOf(httpResponse.getHttpCode()));
+            writer.newLine();
+
+            writer.write("[ContentType]");
+            writer.newLine();
+            writer.write(httpResponse.getContentType());
+            writer.newLine();
+        } finally {
+            writer.close();
+        }
+    }
+
+    private void writeResponseEntity(final HttpResponse httpResponse, final File httpResponseEntityFile) throws IOException {
+        if (httpResponse.getContent() != null) {
+            FileUtils.writeByteArrayToFile(httpResponseEntityFile, httpResponse.getContent());
+        }
+    }
+
+}
---
+++ src/test/java/com/github/kristofa/test/http/HttpResponseFileWriterImplTest.java
@@ -0,0 +1,43 @@
+package com.github.kristofa.test.http;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+public class HttpResponseFileWriterImplTest {
+
+    private static final String UTF8 = "UTF8";
+
+    private final static String TEMP_DIR = System.getProperty("java.io.tmpdir");
+    private final static String HTTP_RESPONSE_FILENAME = "HttpResponseFileWriterImplTest.txt";
+    private final static String HTTP_RESPONSE_ENTITY_FILENAME = "HttpResponseFileWriterImplTest_entity.txt";
+
+    private static final int HTTP_CODE = 200;
+    private static final String CONTENTTYPE = "contentType";
+    private static final byte[] RESPONSE_CONTENT = new String("responseContent").getBytes();
+
+    @Test
+    public void testWrite() throws IOException {
+        final HttpResponseImpl httpResponseImpl = new HttpResponseImpl(HTTP_CODE, CONTENTTYPE, RESPONSE_CONTENT);
+
+        final File responseFile = new File(TEMP_DIR, HTTP_RESPONSE_FILENAME);
+        final File responseEntityFile = new File(TEMP_DIR, HTTP_RESPONSE_ENTITY_FILENAME);
+
+        final HttpResponseFileWriterImpl writer = new HttpResponseFileWriterImpl();
+        writer.write(httpResponseImpl, responseFile, responseEntityFile);
+
+        final String expectedResponseFileContent = "[HttpCode]\n" + //
+            HTTP_CODE + "\n" + //
+            "[ContentType]\n" + //
+            CONTENTTYPE + "\n";
+
+        assertEquals(expectedResponseFileContent, FileUtils.readFileToString(responseFile, UTF8));
+        assertArrayEquals(RESPONSE_CONTENT, FileUtils.readFileToByteArray(responseEntityFile));
+    }
+
+}
