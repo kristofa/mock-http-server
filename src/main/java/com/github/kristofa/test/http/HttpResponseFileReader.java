d8bd5946015ec6a7838decdce9ccf728e2e7db83
Kristof A <kr_adr@yahoo.co.uk>
2012-12-17 19:01:16 +0100

    Add HttpResponseFileReader and HttpResponseFileWriter interfaces.

---
+++ src/main/java/com/github/kristofa/test/http/HttpResponseFileReader.java
@@ -0,0 +1,23 @@
+package com.github.kristofa.test.http;
+
+import java.io.File;
+
+/**
+ * Reads a {@link HttpResponse} from file and reconstructs it.
+ * 
+ * @see HttpResponseFileWriter
+ * @author kristof
+ */
+public interface HttpResponseFileReader {
+
+    /**
+     * Reqds a {@link HttpResponse} that is stored in file(s) and reconstructs it.
+     * 
+     * @param httpResponseFile File in which all http response properties are stored except entity.
+     * @param httpResponseEntityFile File in which http response entity is stored. Entity is optional. If given file does not
+     *            exist entity in returned response will be <code>null</code>.
+     * @return Initialized Http Response.
+     */
+    HttpResponse read(final File httpResponseFile, final File httpResponseEntityFile);
+
+}
---
+++ src/main/java/com/github/kristofa/test/http/HttpResponseFileWriter.java
@@ -0,0 +1,23 @@
+package com.github.kristofa.test.http;
+
+import java.io.File;
+
+/**
+ * Writes a {@link HttpResponse} to file.
+ * 
+ * @see HttpResponseFileReader
+ * @author kristof
+ */
+public interface HttpResponseFileWriter {
+
+    /**
+     * Writes a {@link HttpResponse} to file.
+     * 
+     * @param response The http response to persist.
+     * @param httpResponseFile File in which all http response properties will be stored except entity.
+     * @param httpResponseEntityFile File in which http response entity will be stored. Entity is optional. If http response
+     *            does not have entity this file will not be created.
+     */
+    void write(final HttpResponse response, final File httpResponseFile, final File httpResponseEntityFile);
+
+}
