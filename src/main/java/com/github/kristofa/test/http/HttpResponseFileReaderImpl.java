6d2b128f7c68796c5a6e7e09bfc506cad342d8a9
Kristof A <kr_adr@yahoo.co.uk>
2012-12-23 14:51:34 +0100

    Implement HttpResponseFileReaderImpl.

---
+++ src/main/java/com/github/kristofa/test/http/HttpResponseFileReaderImpl.java
@@ -0,0 +1,63 @@
+package com.github.kristofa.test.http;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Builds a {@link HttpResponse} for which the content is stored on disk. It can reconstruct a {@link HttpResponse} which was
+ * previously stored with {@link HttpResponseFileWriterImpl}.
+ * 
+ * @see HttpResponseFileWriterImpl
+ * @author kristof
+ */
+class HttpResponseFileReaderImpl implements HttpResponseFileReader {
+
+    private final static String HTTPCODE = "[HttpCode]";
+    private final static String CONTENTTYPE = "[ContentType]";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public HttpResponse read(final File httpResponseFile, final File httpResponseEntityFile) {
+
+        try {
+            return readResponse(httpResponseFile, httpResponseEntityFile);
+        } catch (final IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    private HttpResponse readResponse(final File httpResponseFile, final File httpResponseEntityFile) throws IOException {
+        final BufferedReader reader =
+            new BufferedReader(new InputStreamReader(new FileInputStream(httpResponseFile), "UTF-8"));
+        try {
+            readNextLine(reader, HTTPCODE);
+            final int httpCode = Integer.valueOf(reader.readLine());
+            readNextLine(reader, CONTENTTYPE);
+            final String contentType = reader.readLine();
+            byte[] entity = null;
+            if (httpResponseEntityFile.exists()) {
+                entity = FileUtils.readFileToByteArray(httpResponseEntityFile);
+            }
+            return new HttpResponseImpl(httpCode, contentType, entity);
+        } finally {
+            reader.close();
+        }
+    }
+
+    private String readNextLine(final BufferedReader reader, final String expectedValue) throws IOException {
+        final String value = reader.readLine();
+        if (!expectedValue.equals(value)) {
+            throw new IllegalStateException("Unexpected value. Expected " + expectedValue + " but was " + value);
+        }
+        return value;
+    }
+
+}
---
+++ src/test/java/com/github/kristofa/test/http/HttpResponseFileReaderImplTest.java
@@ -0,0 +1,56 @@
+package com.github.kristofa.test.http;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HttpResponseFileReaderImplTest {
+
+    private final static String TEST_FILE_DIRECTORY = "target/test-classes/";
+
+    private HttpResponseFileReaderImpl reader;
+
+    @Before
+    public void setup() {
+        reader = new HttpResponseFileReaderImpl();
+    }
+
+    @Test
+    public void testReadValidFile() {
+        final HttpResponse response =
+            reader.read(new File(TEST_FILE_DIRECTORY, "HttpResponseFileReaderImplTest_valid_file.txt"), new File(
+                TEST_FILE_DIRECTORY, "HttpResponseFileReaderImplTest_entity_valid_file.txt"));
+        assertEquals(200, response.getHttpCode());
+        assertEquals("application/json", response.getContentType());
+        assertNotNull(response.getContent());
+    }
+
+    @Test
+    public void testReadValidFileNoEntity() {
+        final HttpResponse response =
+            reader.read(new File(TEST_FILE_DIRECTORY, "HttpResponseFileReaderImplTest_valid_file.txt"), new File(
+                TEST_FILE_DIRECTORY, "HttpResponseFileReaderImplTest_no_entity.txt"));
+        assertEquals(200, response.getHttpCode());
+        assertEquals("application/json", response.getContentType());
+        assertNull(response.getContent());
+    }
+
+    @Test
+    public void testReadInvalidFile() {
+
+        try {
+            reader.read(new File(TEST_FILE_DIRECTORY, "HttpResponseFileReaderImplTest_invalid_file.txt"), new File(
+                TEST_FILE_DIRECTORY, "HttpResponseFileReaderImplTest_entity_invalid_file.txt"));
+            fail("Expected exception.");
+        } catch (final IllegalStateException e) {
+            assertEquals("Unexpected value. Expected [HttpCode] but was 200", e.getMessage());
+        }
+    }
+
+}
