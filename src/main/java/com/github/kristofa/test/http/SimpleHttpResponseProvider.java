da61ff377de16376d7e6f3e28a6c1ffff22f9a28
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2013-04-30 11:07:23 +0200

    Update SimpleHttpResponseProvider so it only checks for method, path, content type http header and content. If ignores other http headers.

--- src/main/java/com/github/kristofa/test/http/SimpleHttpResponseProvider.java
+++ src/main/java/com/github/kristofa/test/http/SimpleHttpResponseProvider.java
@@ -6,20 +6,30 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 /**
- * {@link HttpResponseProvider} that keeps expected request/responses in memory.
- * <p>
- * Simple to use for not too complex request/responses.
+ * {@link HttpResponseProvider} that keeps expected request/responses in memory. Its purpose is to match simple requests that
+ * can be easily programmatically defined in code. It supports matching:
+ * <p/>
+ * <ul>
+ * <li>HTTP Method (GET, PUT, POST, DELETE)</li>
+ * <li>Path</li>
+ * <li>Content-Type http header</li>
+ * <li>Entity</li>
+ * </ul>
  * 
  * @see MockHttpServer
  * @author kristof
  */
 public class SimpleHttpResponseProvider implements HttpResponseProvider {
 
-    private HttpRequestImpl latestRequest;
+    private LimitedHttpRequestData latestRequest;
 
-    private final Map<HttpRequest, HttpResponse> expectedRequests = new HashMap<HttpRequest, HttpResponse>();
-    private final Set<HttpRequest> receivedRequests = new HashSet<HttpRequest>();
+    private final Map<LimitedHttpRequestData, HttpResponse> expectedRequests =
+        new HashMap<LimitedHttpRequestData, HttpResponse>();
+    private final Set<LimitedHttpRequestData> receivedRequests = new HashSet<LimitedHttpRequestData>();
 
     /**
      * Provide an expected request with content.
@@ -32,9 +42,12 @@ public class SimpleHttpResponseProvider implements HttpResponseProvider {
      */
     public SimpleHttpResponseProvider expect(final Method method, final String path, final String contentType,
         final String data) {
-        latestRequest = new HttpRequestImpl();
-        latestRequest.method(method).path(path).content(data.getBytes())
-            .httpMessageHeader(HttpMessageHeaderField.CONTENTTYPE.getValue(), contentType);
+        latestRequest = new LimitedHttpRequestData();
+        latestRequest.setMethod(method);
+        latestRequest.setPath(path);
+        latestRequest.setContent(data.getBytes());
+        latestRequest.setContentType(contentType);
+
         return this;
     }
 
@@ -46,8 +59,9 @@ public class SimpleHttpResponseProvider implements HttpResponseProvider {
      * @return current {@link SimpleHttpResponseProvider}. Allows chaining calls.
      */
     public SimpleHttpResponseProvider expect(final Method method, final String path) {
-        latestRequest = new HttpRequestImpl();
-        latestRequest.method(method).path(path);
+        latestRequest = new LimitedHttpRequestData();
+        latestRequest.setMethod(method);
+        latestRequest.setPath(path);
         return this;
     }
 
@@ -70,8 +84,21 @@ public class SimpleHttpResponseProvider implements HttpResponseProvider {
      */
     @Override
     public HttpResponse getResponse(final HttpRequest request) {
-        receivedRequests.add(request);
-        return expectedRequests.get(request);
+
+        final LimitedHttpRequestData limitedRequest = new LimitedHttpRequestData();
+        limitedRequest.setMethod(request.getMethod());
+        limitedRequest.setPath(request.getPath());
+        limitedRequest.setContent(request.getContent());
+
+        for (final HttpMessageHeader header : request.getHttpMessageHeaders()) {
+            if (HttpMessageHeaderField.CONTENTTYPE.getValue().equals(header.getName())) {
+                limitedRequest.setContentType(header.getValue());
+                break;
+            }
+        }
+
+        receivedRequests.add(limitedRequest);
+        return expectedRequests.get(limitedRequest);
     }
 
     /**
@@ -84,21 +111,81 @@ public class SimpleHttpResponseProvider implements HttpResponseProvider {
 
             final Collection<HttpRequest> missing = new HashSet<HttpRequest>();
 
-            for (final HttpRequest expectedRequest : expectedRequests.keySet()) {
+            for (final LimitedHttpRequestData expectedRequest : expectedRequests.keySet()) {
                 if (!receivedRequests.contains(expectedRequest)) {
-                    missing.add(expectedRequest);
+                    missing.add(httpRequestFor(expectedRequest));
                 }
             }
 
             final Collection<HttpRequest> unexpected = new HashSet<HttpRequest>();
-            for (final HttpRequest receivedRequest : receivedRequests) {
+            for (final LimitedHttpRequestData receivedRequest : receivedRequests) {
                 if (!expectedRequests.keySet().contains(receivedRequest)) {
-                    unexpected.add(receivedRequest);
+                    unexpected.add(httpRequestFor(receivedRequest));
                 }
             }
 
             throw new UnsatisfiedExpectationException(missing, unexpected);
 
+        }
+
+    }
+
+    private HttpRequest httpRequestFor(final LimitedHttpRequestData requestData) {
+        final HttpRequestImpl httpRequestImpl = new HttpRequestImpl();
+        httpRequestImpl.method(requestData.getMethod()).path(requestData.getPath()).content(requestData.getContent());
+        if (requestData.getContentType() != null) {
+            httpRequestImpl.httpMessageHeader(HttpMessageHeaderField.CONTENTTYPE.getValue(), requestData.getContentType());
+        }
+        return httpRequestImpl;
+    }
+
+    private class LimitedHttpRequestData {
+
+        private Method method;
+        private String path;
+        private String contentType;
+        private byte[] content;
+
+        public Method getMethod() {
+            return method;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public String getContentType() {
+            return contentType;
+        }
+
+        public byte[] getContent() {
+            return content;
+        }
+
+        public void setMethod(final Method method) {
+            this.method = method;
+        }
+
+        public void setPath(final String path) {
+            this.path = path;
+        }
+
+        public void setContentType(final String contentType) {
+            this.contentType = contentType;
+        }
+
+        public void setContent(final byte[] content) {
+            this.content = content;
+        }
+
+        @Override
+        public int hashCode() {
+            return HashCodeBuilder.reflectionHashCode(this, false);
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            return EqualsBuilder.reflectionEquals(this, obj, false);
         }
 
     }
