8ddfb2d9dc18222e67dc962bfb81df3076ff0483
Kristof A <kr_adr@yahoo.co.uk>
2013-12-28 11:16:36 +0100

    Add package javadoc for file package.

---
+++ src/main/java/com/github/kristofa/test/http/file/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides implementations for storing http request/responses to disk using
+ * {@link com.github.kristofa.test.http.LoggingHttpProxy} and replaying them using
+ * {@link com.github.kristofa.test.http.MockHttpServer}. </p>
+ */
+package com.github.kristofa.test.http.file;
