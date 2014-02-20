a072f8879856b530705c53770344e153cf9934de
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2014-02-20 21:18:44 +0100

    Support removal of previous logged requests/responses before logging first request/response.
    This prevents potential test failures if your new log session has less
    request/responses as your previous one or if the requests/responses
    come in different order.

--- src/main/java/com/github/kristofa/test/http/file/HttpRequestResponseFileLoggerFactory.java
+++ src/main/java/com/github/kristofa/test/http/file/HttpRequestResponseFileLoggerFactory.java
@@ -1,8 +1,11 @@
 package com.github.kristofa.test.http.file;
 
+import java.io.File;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.github.kristofa.test.http.HttpRequestResponseLogger;
 import com.github.kristofa.test.http.HttpRequestResponseLoggerFactory;
@@ -17,11 +20,27 @@ import com.github.kristofa.test.http.LoggingHttpProxy;
  */
 public class HttpRequestResponseFileLoggerFactory implements HttpRequestResponseLoggerFactory {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpRequestResponseFileLoggerFactory.class);
+
     private final AtomicInteger atomicInteger = new AtomicInteger();
     private final String directory;
     private final String fileName;
     private final HttpRequestFileWriter requestWriter;
     private final HttpResponseFileWriter responseWriter;
+    private final boolean deleteExistingFiles;
+    private boolean firstRequest = true;
+
+    /**
+     * Creates a new instance. We will not delete any existing files.
+     * 
+     * @param directory Target directory in which to store request/responses. Directory should already exist. Should not be
+     *            <code>null</code> or blank.
+     * @param fileName Base file name. Should not contain extension. Will be suffixed with sequence number and .txt
+     *            extension. Should not be <code>null</code> or blank.
+     */
+    public HttpRequestResponseFileLoggerFactory(final String directory, final String fileName) {
+        this(directory, fileName, false);
+    }
 
     /**
      * Creates a new instance.
@@ -30,12 +49,17 @@ public class HttpRequestResponseFileLoggerFactory implements HttpRequestResponse
      *            <code>null</code> or blank.
      * @param fileName Base file name. Should not contain extension. Will be suffixed with sequence number and .txt
      *            extension. Should not be <code>null</code> or blank.
+     * @param deleteExistingFiles If value is <code>true</code> we will delete all existing files prior to logging new
+     *            requests. This is often helpful because if we have less requests than before otherwise old files keep on
+     *            lingering which can cause failing tests.
      */
-    public HttpRequestResponseFileLoggerFactory(final String directory, final String fileName) {
+    public HttpRequestResponseFileLoggerFactory(final String directory, final String fileName,
+        final boolean deleteExistingFiles) {
         Validate.notBlank(directory);
         Validate.notBlank(fileName);
         this.directory = directory;
         this.fileName = fileName;
+        this.deleteExistingFiles = deleteExistingFiles;
         requestWriter = new HttpRequestFileWriterImpl();
         responseWriter = new HttpResponseFileWriterImpl();
     }
@@ -45,7 +69,52 @@ public class HttpRequestResponseFileLoggerFactory implements HttpRequestResponse
      */
     @Override
     public HttpRequestResponseLogger getHttpRequestResponseLogger() {
+
+        if (firstRequest && deleteExistingFiles) {
+            synchronized (atomicInteger) {
+                if (firstRequest) {
+                    deleteExistingFiles();
+                    firstRequest = false;
+                }
+
+            }
+        }
+
         return new HttpRequestResponseFileLogger(directory, fileName, atomicInteger.incrementAndGet(), requestWriter,
             responseWriter);
     }
+
+    private void deleteExistingFiles() {
+
+        int seqNr = 1;
+        while (true) {
+            final String requestFileName = FileNameBuilder.REQUEST_FILE_NAME.getFileName(fileName, seqNr);
+            if (FileDeleteStatus.DID_NOT_EXIST.equals(delete(directory, requestFileName))) {
+                return;
+            }
+            final String requestEntityFileName = FileNameBuilder.REQUEST_ENTITY_FILE_NAME.getFileName(fileName, seqNr);
+            delete(directory, requestEntityFileName);
+            final String responseFileName = FileNameBuilder.RESPONSE_FILE_NAME.getFileName(fileName, seqNr);
+            delete(directory, responseFileName);
+            final String responseEntityFileName = FileNameBuilder.RESPONSE_ENTITY_FILE_NAME.getFileName(fileName, seqNr);
+            delete(directory, responseEntityFileName);
+            seqNr++;
+        }
+    }
+
+    private FileDeleteStatus delete(final String directory, final String fileName) {
+        final File file = new File(directory, fileName);
+        if (file.exists()) {
+            if (!file.delete()) {
+                LOGGER.warn("Unable to delete " + file);
+                return FileDeleteStatus.DELETE_FAILED;
+            }
+            return FileDeleteStatus.DELETE_SUCCES;
+        }
+        return FileDeleteStatus.DID_NOT_EXIST;
+    }
+
+    private enum FileDeleteStatus {
+        DID_NOT_EXIST, DELETE_SUCCES, DELETE_FAILED
+    };
 }
--- src/test/java/com/github/kristofa/test/http/file/HttpRequestResponseFileLoggerFactoryTest.java
+++ src/test/java/com/github/kristofa/test/http/file/HttpRequestResponseFileLoggerFactoryTest.java
@@ -6,16 +6,29 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import com.github.kristofa.test.http.HttpRequestImpl;
 import com.github.kristofa.test.http.HttpRequestResponseLogger;
-import com.github.kristofa.test.http.file.HttpRequestResponseFileLogger;
-import com.github.kristofa.test.http.file.HttpRequestResponseFileLoggerFactory;
+import com.github.kristofa.test.http.HttpResponseImpl;
+import com.github.kristofa.test.http.Method;
 
 public class HttpRequestResponseFileLoggerFactoryTest {
 
-    private final static String DIRECTORY = "/tmp";
+    private final static String DIRECTORY = "target/";
     private final static String FILE_NAME = "testFile";
+
+    @Before
+    public void setup() throws IOException {
+        deleteFiles(1);
+        deleteFiles(2);
+    }
 
     @Test
     public void testGetHttpRequestResponseLogger() {
@@ -39,7 +52,90 @@ public class HttpRequestResponseFileLoggerFactoryTest {
             fileLogger2.getRequestFileWriter());
         assertSame("We expect that underlying response file writer is singleton.", fileLogger.getResponseFileWriter(),
             fileLogger2.getResponseFileWriter());
+    }
 
+    @Test
+    public void testDeleteExistingFiles() throws IOException {
+        final List<File> files = new ArrayList<File>();
+        files.addAll(createFiles(1));
+        files.addAll(createFiles(2));
+
+        final HttpRequestResponseFileLoggerFactory factory =
+            new HttpRequestResponseFileLoggerFactory(DIRECTORY, FILE_NAME, true);
+        assertNotNull(factory.getHttpRequestResponseLogger());
+
+        for (final File file : files) {
+            assertFalse("We expected all files to be deleted. Was not deleted: " + file, file.exists());
+        }
+    }
+
+    @Test
+    public void testDontDeleteExistingFiles() throws IOException {
+        final List<File> files = new ArrayList<File>();
+        files.addAll(createFiles(1));
+        files.addAll(createFiles(2));
+
+        final HttpRequestResponseFileLoggerFactory factory =
+            new HttpRequestResponseFileLoggerFactory(DIRECTORY, FILE_NAME, false);
+        assertNotNull(factory.getHttpRequestResponseLogger());
+
+        for (final File file : files) {
+            assertTrue("We expected all files to be still existing. Does not exist:" + file, file.exists());
+        }
+    }
+
+    @Test
+    public void testLogSingleRequestAfterDeleteExistingFiles() throws IOException {
+        final List<File> files = new ArrayList<File>();
+        files.addAll(createFiles(1));
+        files.addAll(createFiles(2));
+
+        final HttpRequestResponseFileLoggerFactory factory =
+            new HttpRequestResponseFileLoggerFactory(DIRECTORY, FILE_NAME, true);
+        final HttpRequestResponseLogger logger = factory.getHttpRequestResponseLogger();
+        final HttpRequestImpl request = new HttpRequestImpl();
+        request.method(Method.POST).path("/").content(new String("content").getBytes());
+        logger.log(request);
+        final HttpResponseImpl response = new HttpResponseImpl(200, "application/json", "{}".getBytes());
+        logger.log(response);
+
+        for (final File file : getFiles(1)) {
+            assertTrue("Those files should exist, should be recreated." + file, file.exists());
+        }
+        for (final File file : getFiles(2)) {
+            assertFalse("Those files should have been deleted." + file, file.exists());
+        }
+    }
+
+    private List<File> createFiles(final int seqNr) throws IOException {
+        final List<File> fileList = getFiles(seqNr);
+        for (final File file : fileList) {
+            file.createNewFile();
+        }
+        return fileList;
+    }
+
+    private List<File> deleteFiles(final int seqNr) throws IOException {
+        final List<File> fileList = getFiles(seqNr);
+        for (final File file : fileList) {
+            if (file.exists() && !file.delete()) {
+                throw new IllegalStateException("Unable to delete file " + file);
+            }
+        }
+        return fileList;
+    }
+
+    private List<File> getFiles(final int seqNr) {
+        final List<File> fileList = new ArrayList<File>();
+        final String requestFileName = FileNameBuilder.REQUEST_FILE_NAME.getFileName(FILE_NAME, seqNr);
+        fileList.add(new File(DIRECTORY, requestFileName));
+        final String requestEntityFileName = FileNameBuilder.REQUEST_ENTITY_FILE_NAME.getFileName(FILE_NAME, seqNr);
+        fileList.add(new File(DIRECTORY, requestEntityFileName));
+        final String responseFileName = FileNameBuilder.RESPONSE_FILE_NAME.getFileName(FILE_NAME, seqNr);
+        fileList.add(new File(DIRECTORY, responseFileName));
+        final String responseEntityFileName = FileNameBuilder.RESPONSE_ENTITY_FILE_NAME.getFileName(FILE_NAME, seqNr);
+        fileList.add(new File(DIRECTORY, responseEntityFileName));
+        return fileList;
     }
 
 }
